132783d2d3996e9e88aac1851e7a2d623f49db06
sidd-oo <sahoosiddharth3@gmail.com>
2022-02-24 22:52:50 +0530

    feat: writing a graphql schema

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const app = express();
 
+app.use("/graphql", graphqlHTTP({
+    
+}) )
+
 app.listen(8000, ()=>{
     console.log("Server is running on port 8000");
 })
